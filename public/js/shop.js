3e69a7a0f09814848813350c5b5f2c9e0a986f81
Roman Rios <romanrios@live.com>
2023-12-14 16:17:07 -0300

    let

--- public/js/shop.js
+++ public/js/shop.js
@@ -1,7 +1,8 @@
 // links menú "ORDENAR POR"
+// esta función se dispara desde el HTML
 function redirectToSelected() {
-    var selectElement = document.getElementById("orderby");
-    var selectedValue = selectElement.options[selectElement.selectedIndex].value;
+    let selectElement = document.getElementById("orderby");
+    let selectedValue = selectElement.options[selectElement.selectedIndex].value;
 
     // Redirigir a la ruta seleccionada
     if (selectedValue) {
